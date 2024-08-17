d8dc81c530835b928ec5f99a24e09f0edacc1c8d
Ann Maria Jose <annmariajose1412@gmail.com>
2024-08-17 10:31:23 -0500

    Add redux

---
+++ theme-app/src/redux/reducers.js
@@ -0,0 +1,13 @@
+import * as actions from "./actions";
+import { combnieReducers } from "redux";
+
+const preferences = (state = { darkThemeEnabled: false }, action) => {
+    switch(action.type) {
+        case actions.TOGGLE_DARKTHEME:
+            return { ...state, darkThemeEnabled: !state.darkThemeEnabled };
+        default:
+            return state;
+    }
+};
+
+export default combnieReducers({ preferences });
