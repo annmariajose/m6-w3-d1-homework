1786c8f9890b411510fcf283997c6b67f0c13863
Ann Maria Jose <annmariajose1412@gmail.com>
2024-08-17 10:20:24 -0500

    Add themes to app

--- theme-app/src/App.js
+++ theme-app/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "styled-theming";
 
-function App() {
+export const theme1 = theme("theme", {
+  light: "#fff",
+  dark: "#2d2d2d",
+});
+
+export const theme2 = theme("theme", {
+  light: "#000",
+  dark: "#fff",
+});
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+  background-color: ${theme1};
+  `;
+
+const SwitchTheme = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 75px;
+  background-color: ${theme2};
+  `;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <ThemeProvider theme={{ theme: "light" }}>
+        <SwitchTheme>
+          <h1>Theme App</h1>
+          <p>
+            <input type="checkbox" />Use Dark Theme
+          </p>
+        </SwitchTheme>
+        <Container>
+          <h2>Welcome!</h2>
+          <h3>Full Stack Web Development</h3>
+        </Container>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
